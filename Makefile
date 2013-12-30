591fa36cf4e73f01a666f67549ba3596dcaf9a84
MrYangYang <lys701@gmail.com>
2013-12-30 23:50:54 +0800

    code optimize.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC = gcc
-LIBS = 
-CFLAGS = 
+LIBS = `pkg-config --libs glib-2.0`
+CFLAGS = `pkg-config --cflags glib-2.0`
 
 myls: myls.o
 	gcc -g -Wall myls.o -o myls
--- myls.c
+++ myls.c
@@ -1,8 +1,14 @@
+#include <glib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <argp.h>
+#include <sys/ioctl.h>
+#include <dirent.h>
 
+/**
+ * command line args structure
+ */
 struct arguments
 {
     char *arg[1]; // dir or file path
@@ -11,6 +17,24 @@ struct arguments
     char *dvalue; // -dXXX 
 };
 
+/**
+ * get the width of terminal in ascii char.
+ */
+int get_winsize_width()
+{
+    struct winsize size;
+    int input_fd = 0;
+    if(ioctl(input_fd, TIOCGWINSZ, &size) == -1) {
+        fprintf(stderr, "cannot get terminal size.\n");
+        exit(-1);
+    } 
+    return size.ws_col;
+}
+
+/**
+ * return non-zero value if all char in str is in [1-9]
+ * zero otherwise.
+ */
 int isNumbers(char *str)
 {
     int len = strlen(str);
@@ -23,6 +47,10 @@ int isNumbers(char *str)
     return 1;
 }
 
+
+/**
+ * help text of command line args.
+ */
 static struct argp_option options[] = 
 {
     {"list", 'l', 0, 0, "list all files and dirs exclude hides"}, 
@@ -32,6 +60,9 @@ static struct argp_option options[] =
     {0}
 };
 
+/**
+ * parse args to arguments struct.
+ */
 static error_t parse_opt(int key, char *arg, struct argp_state *state)
 {
     struct arguments *arguments = state->input;
@@ -72,15 +103,49 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state)
     return 0;
 }
 
+size_t get_dir_file_num(const char *path)
+{
+    DIR *dir = opendir(path);
+    if(!dir){
+        fprintf(stderr, "read file num of dir %s err\n", path);
+    }
+    size_t count = 0;
+    struct dirent *cur_node = NULL;
+    while((cur_node = readdir(dir)) != NULL){
+        if(!strcmp(".", cur_node->d_name) 
+                || !strcmp("..", cur_node->d_name)){
+            continue;
+        }
+        count++;
+    }
+
+    return count;
+}
+
+struct _myls_info {
+    DIR *dir;
+    struct dirent *cur_node;
+    char *base_path;
+    GList *target;
+};
+
+typedef struct _myls_info myls_info;
+
+int read_next(myls_info *ls_info);
+
+
 static char args_doc[] = "dir";
 
 static char doc[] = "a program like ls";
 
 static struct argp argp = {options, parse_opt, args_doc, doc};
 
+static int shell_width;
 
 int main(int argc, char **argv)
 {
+    shell_width = get_winsize_width();
+    
     struct arguments arguments;
 
     arguments.arg[0] = NULL;
@@ -93,6 +158,56 @@ int main(int argc, char **argv)
     if(!arguments.arg[0]){
         arguments.arg[0] = "./";
     }
+
+    GList *elemlist = NULL; // 文件列表
+    DIR *basedir = opendir(arguments.arg[0]);
+    if(basedir == NULL){
+        fprintf(stderr, "opendir %s failed.\n", arguments.arg[0]);
+    }
+
+    struct dirent *cur_node = NULL;
+    int dvalue_int = 0;
+
+    if(arguments.dvalue){
+        dvalue_int = atoi(arguments.dvalue);
+    }
+
+    /*GList *temp_list = NULL;
+    GString *temp_name = NULL;
+    GString *temp_path = NULL;
+    struct stat info;
+    size_t temp_count;
+    while((cur_node = readdir(basedir)) != NULL){
+
+        // for . & ..
+        if(!strcmp(".", cur_node->d_name)
+                || !strcmp("..", cur_node->d_name)){
+            continue;
+        }
+
+        // read dir
+        temp_name = g_string_new(cur_node->d_name);
+        
+        // if has argument -d
+        if(arguments.dvalue){
+            temp_path = g_string_new(arguments.arg[0]);
+            temp_path = g_string_append(temp_path, cur_node->d_name);
+            stat(temp_path->str, &info);
+            
+            // if current node is a dir.
+            if(S_ISDIR(info.st_mode)){
+                temp_count = get_dir_file_num(temp_path->str);
+                
+                // if current node has more than dvalue files
+                if(temp_count >= dvalue_int)            
+                    temp_list = g_list_append(temp_list, temp_name);
+            }
+
+            continue;
+        } else {
+            
+        }
+    }*/
 
     printf("dir %s\n", arguments.arg[0]);
     printf("lflag = %d, aflag = %d\n", arguments.lflag, arguments.aflag);
